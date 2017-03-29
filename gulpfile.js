fd77213675e1fa1e7a6e2ee7bba650e7c2a9691a
Juan Balbuena <ianivigo2@gmail.com>
2017-03-29 19:45:31 +0200

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@
 var postcss = require('gulp-postcss');
 var gulp = require('gulp');
 var cssnano = require('cssnano');
+var doImport = require("postcss-import");
 var cssnext = require("postcss-cssnext");
 var cssnested  = require("postcss-nested");
 var server = require("gulp-server-livereload");
@@ -12,11 +13,12 @@ var watch = require('gulp-watch');
 
 gulp.task('css', function () {
     var plugins = [
+        doImport(),
         cssnested(),
         cssnext({browsers: ['last 2 version']}),
         cssnano()
     ];
-    return gulp.src('./src/css/*.css')
+    return gulp.src('./src/css/style.css')
         .pipe(postcss(plugins))
         .pipe(gulp.dest('./build/css'));
 });
