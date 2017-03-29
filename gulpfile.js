e415f17954f60f675d09ae4fbed49c6349872b26
Juan Balbuena <ianivigo2@gmail.com>
2017-03-29 18:07:46 +0200

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Juan on 29/03/2017.
+ */
+
+var postcss = require('gulp-postcss');
+var gulp = require('gulp');
+var cssnano = require('cssnano');
+var cssnext = require("postcss-cssnext");
+var cssnested  = require("postcss-nested");
+var server = require("gulp-server-livereload");
+var watch = require('gulp-watch');
+
+gulp.task('css', function () {
+    var plugins = [
+        cssnested(),
+        cssnext({browsers: ['last 2 version']}),
+        cssnano()
+    ];
+    return gulp.src('./src/css/*.css')
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('server', function() {
+    gulp.src('./')
+        .pipe(server({
+            livereload: true,
+            defaultFile: 'index.html',
+            directoryListing: true,
+            open: true
+        }));
+});
+
+gulp.task("watch", function () {
+    gulp.watch("./src/css/*.css", ["css"]);
+});
+
+gulp.task("default", ["watch", "server"]);
